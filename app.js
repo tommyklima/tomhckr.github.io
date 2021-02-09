02ecdc4329fb1405b42627427ebb0900bc169b17
Tom Hckr <tomhckr.tech@gmail.com>
2021-02-09 17:43:39 +0100

    I applied javascript scroller on my site

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-const titleText = document.querySelector('.intro-highlight');
+// navbar scrollers
+const socialsNav = document.querySelectorAll('.nav-element')[1];
+socialsNav.addEventListener('click', function () {
+    document.querySelector('#social').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+const projectsNav = document.querySelectorAll('.nav-element')[2];
+projectsNav.addEventListener('click', function () {
+    document.querySelector('#projects').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
