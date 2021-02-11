96c264328d89637690938da2a69ffb35b53b8940
TomHckr <tomhckr.tech@gmail.com>
2021-02-11 19:02:24 +0100

    Updating the project hierarchy I have improved the directories organisation and changed the organisation.

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 // navbar scrollers
 const socialsNav = document.querySelectorAll('.nav-element')[1];
+const projectsNav = document.querySelectorAll('.nav-element')[2];
+
 socialsNav.addEventListener('click', function () {
     document.querySelector('#social').scrollIntoView({
         behavior: 'smooth'
     });
 });
 
-const projectsNav = document.querySelectorAll('.nav-element')[2];
 projectsNav.addEventListener('click', function () {
     document.querySelector('#projects').scrollIntoView({
         behavior: 'smooth'
     });
 });
+
+// darkmode / light mode toggle
+const modeBtn = document.querySelector(".modeBtn");
+
+const changeMode = () => {
+    document.querySelector('.background-body').style.backgroundColor = "#ffffff";
+}
+
+modeBtn.addEventListener('click', changeMode);
